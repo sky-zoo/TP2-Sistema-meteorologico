7ab8f3d1ffddc65dc4a1b802eef6c0fc4fff422f
NoeliaAG <noaguero@fi.uba.ar>
2020-07-22 22:25:29 -0300

    Se agrega menu principal para mergear

--- SistemaMeteorologico.py
+++ SistemaMeteorologico.py
@@ -190,6 +190,7 @@ def imprimir_grafico_humedades(datos_graficos_humedades):
 
     plt.show()
 
+
 def obtener_temperatura_max(lista_historico):
     """
     Precondición: Obtiene temperatura máxima promedio del archivo histórico.
@@ -229,6 +230,7 @@ def obtener_mm_max_lluvia(lista_historico):
 
     return fecha_max_mm_lluvia, max_mm_lluvia
 
+
 def grafico_temperaturas():
     """
     Lee el archivo histórico, obtiene los datos necesarios para graficar e imprime el gráfico de temperaturas.
@@ -238,6 +240,7 @@ def grafico_temperaturas():
     datos_grafico_temperaturas = obtener_datos_grafico_temperaturas(arch_historico)
     imprimir_grafico_temperaturas(datos_grafico_temperaturas)
 
+
 def grafico_humedades():
     """
     Lee el archivo histórico, obtiene los datos necesarios para graficar e imprime el gráfico de humedades.
@@ -245,6 +248,7 @@ def grafico_humedades():
     arch_historico = leer_archivo_historico()
     datos_grafico_humedades = obtener_datos_grafico_humedades(arch_historico)
     imprimir_grafico_humedades(datos_grafico_humedades)
+
 
 def mm_max_lluvia():
     """
@@ -258,6 +262,7 @@ def mm_max_lluvia():
     print(f"Valor máximo  | {max_mm_lluvia}")
     print(f"Fecha         | {fecha_max_mm_lluvia}")
 
+
 def temp_max():
     """
     Lee el archivo histórico, obtiene la temperatura máxima e imprime por pantalla.
@@ -268,3 +273,70 @@ def temp_max():
     print("Medición      | Temperatura")
     print(f"Valor máximo  | {temperatura_max}")
     print(f"Fecha         | {fecha_temp_max}")
+
+
+def imprimir_menu():
+    """
+    Imprime el menu principal de la aplicación.
+    """
+    print("-----------------------------------------")
+    print("           T O R M E N T A               ")
+    print("-----------------------------------------")
+    print("1. Alertas (geolocalización)\n2. Alertas (nacional)\n3. Pronóstico\n4. Datos históricos\n5. Tormetas por radar\n6. Salir")
+
+
+def imprimir_menu_csv():
+    """
+    Imprime el menu para el archivo histórico.
+    """
+    print("\n---------- Datos históricos ----------")
+    print("Datos de los últimos 5 años de:\n1. Promedio temperaturas\n2. Promedio humedad\n3. Milímetros máximos de lluvia\n4. Temperatura máxima\n5. Salir")
+
+
+def menu_csv():
+    opcion_csv = "0"
+    while opcion_csv != "5":
+        imprimir_menu_csv()
+        opcion_csv = input("\nIngrese la opción que desea: ")
+        while opcion_csv.isnumeric() is False or int(opcion_csv) <= 0 or int(opcion_csv) > 5:
+            opcion_csv = input("Por favor, ingrese un número válido: ")
+
+        if opcion_csv == "1":
+            grafico_temperaturas()
+
+        elif opcion_csv == "2":
+            grafico_humedades()
+
+        elif opcion_csv == "3":
+            mm_max_lluvia()
+
+        elif opcion_csv == "4":
+            temp_max()
+
+
+def main():
+    #definir variables
+    opcion_menu = "0"
+
+    while opcion_menu != "6":
+        imprimir_menu()
+        opcion_menu = input("\nIngrese la opción que desea: ")
+        while opcion_menu.isnumeric() is False or int(opcion_menu) <= 0 or int(opcion_menu) > 6:
+            opcion_menu = input("Por favor, ingrese un número válido: ")
+
+        if opcion_menu == "1":
+            print("---------- Alertas por geolocalización ----------")
+
+        elif opcion_menu == "2":
+            print("---------- Alertas a nivel nacional ----------")
+
+        elif opcion_menu == "3":
+            print("---------- Pronóstico extendido ----------")
+
+        elif opcion_menu == "4":
+            menu_csv()
+
+        elif opcion_menu == "5":
+            print("\n---------- Análisis de imagen ----------")
+
+main()
